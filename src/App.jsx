0543f66049f84e81dfd77c5549bbcce996531596
Ben Merlotti <benmerlotti@gmail.com>
2025-08-04 17:49:08 -0400

    Fix routing for GitHub Pages using HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // src/App.jsx
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
 import Layout from './components/Layout';
 import Home from './pages/Home';
@@ -11,17 +11,19 @@ import Video from './pages/Video';
 function App() {
   return (
     <ThemeProvider>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          {/* Home page as the default */}
-          <Route index element={<Home />} />
-          
-          <Route path="about" element={<About />} />
-          <Route path="edit-shoot" element={<Video />} />
-          <Route path="vfx-3d" element={<Vfx3D />} />
-          <Route path="dev" element={<Dev />} />
-        </Route>
-      </Routes>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            {/* Home page as the default */}
+            <Route index element={<Home />} />
+            
+            <Route path="about" element={<About />} />
+            <Route path="edit-shoot" element={<Video />} />
+            <Route path="vfx-3d" element={<Vfx3D />} />
+            <Route path="dev" element={<Dev />} />
+          </Route>
+        </Routes>
+      </HashRouter>
     </ThemeProvider>
   );
 }
