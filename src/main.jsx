e3b8d64241d1a56ef85e6afd1d938bc8c5126c55
Ben Merlotti <benmerlotti@gmail.com>
2025-08-04 17:54:22 -0400

    Fix Router conflict by removing BrowserRouter from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
 // src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter> {/* Wrap your App component */}
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
